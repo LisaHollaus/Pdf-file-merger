0f2ab328594e700ef5921c5cb67d3bdee55dee23
LisaHollaus <23IMC10611@fh-krems.ac.at>
2024-09-09 15:57:06 +0200

    pdf-file-merger

---
+++ main.py
@@ -0,0 +1,34 @@
+import PyPDF2
+import os
+
+# Setting the base directory to the directory containing the PDF files
+base_directory = r"C:\Users\lisah\OneDrive - IMC\IMC\Semester 2"
+
+# Loop through all the directories and subdirectories in the base directory
+for root, dirs, files in os.walk(base_directory):
+    # make sure there are no merged PDFs in the current directory
+    # if there are, remove them, so they don't get merged again
+    if "merged-pdfs.pdf" in files:
+        files.remove("merged-pdfs.pdf")
+
+    # if there are less than 2 pdfs in the current directory, skip it
+    if len([file for file in files if file.endswith(".pdf")]) < 2:
+        continue
+
+    # Create a PdfMerger object
+    merger = PyPDF2.PdfMerger()
+
+    # Loop through all the files in the current directory
+    for file in files:
+        if file.endswith(".pdf"):
+            try:
+                merger.append(os.path.join(root, file))
+            except PyPDF2.errors.PdfReadError:
+                print(f"Could not read file: {file}")
+                continue
+
+    # Write the merged PDF to a file if it's not empty
+    if merger.pages:
+        merger.write(os.path.join(root, "merged-pdfs.pdf"))
+
+    merger.close()
